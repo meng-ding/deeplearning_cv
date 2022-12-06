8a6f90b66d251f99a9c3ce2aa66cde18798892ee
meng-ding <mengding@buffalo.edu>
2022-12-06 02:08:15 -0500

    animals snn

--- chap12/shallownet_animals.py
+++ chap12/shallownet_animals.py
@@ -9,8 +9,9 @@ from pyimagesearch.nn.conv import ShallowNet
 from keras.optimizers import SGD
 from imutils import paths
 import matplotlib.pyplot as plt
-import numpy
+import numpy as np
 import argparse
+# import gdp
 
 # construct the argument parser and parse the argument
 ap = argparse.ArgumentParser()
@@ -65,13 +66,14 @@ plt.style.use("ggplot")
 plt.figure()
 plt.plot(np.arange(0, 100), H.history["loss"], label="train_loss")
 plt.plot(np.arange(0, 100), H.history["val_loss"], label="val_loss")
-plt.plot(np.arange(0, 100), H.history["acc"], label="train_acc")
-plt.plot(np.arange(0, 100), H.history["val_acc"], label="val_acc")
+plt.plot(np.arange(0, 100), H.history["accuracy"], label="train_acc")
+plt.plot(np.arange(0, 100), H.history["val_accuracy"], label="val_acc")
 plt.title("Training Loss and Accuracy")
 plt.xlabel("Epoch #")
 plt.ylabel("Loss/Accuracy")
 plt.legend()
 plt.show()
+plt.savefig('snn_animals.png')
 
 
 
